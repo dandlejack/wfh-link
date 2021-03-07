308b968583677d6d6f26386a1a27bcfc738c5a45
dandlejack <dandlejack@gmail.com>
2021-03-07 21:53:59 +0700

    change ip server

--- server.configs.js
+++ server.configs.js
@@ -1,4 +1,4 @@
 export const BACKEND_API =
-  process.env.REACT_APP_BACKEND_API || 'http://localhost:4000' || 'http://34.67.95.162'  
+  process.env.REACT_APP_BACKEND_API || 'http://api.หาคนโพส.com'
 
   //admin role = 4y0h9WnLw/TjWXpwK9EZ4D7WCZaB9s/2U/sPcnup1do=
